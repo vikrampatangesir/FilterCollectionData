98ca8b53b166a8c3dc1c735f6f7ce166edcf7a23
vikram <vikram@DESKTOP-JKERAUU>
2023-07-17 10:15:42 +0530

    filter opration

---
+++ src/Product.java
@@ -0,0 +1,13 @@
+
+public class Product {
+
+	 int id;  
+	    String name;  
+	    float price;  
+	    public Product(int id, String name, float price) {  
+	        super();  
+	        this.id = id;  
+	        this.name = name;  
+	        this.price = price;  
+	    }  
+}  
